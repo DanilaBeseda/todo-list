39b5b5c8bb9659f14282108a5e4a06ded49b33e3
Beseda Danila <d.beseda@ylab.io>
2022-11-16 18:17:59 +0300

    Initialize firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyC5_WkDtVKKmUReDfh0aSykE7JZLHEJcPU",
+  authDomain: "todo-6a9dd.firebaseapp.com",
+  databaseURL:
+    "https://todo-6a9dd-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todo-6a9dd",
+  storageBucket: "todo-6a9dd.appspot.com",
+  messagingSenderId: "893867700739",
+  appId: "1:893867700739:web:8a45a5587da027cc581436",
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { config } from "./firebaseConfig";
+
+//Initialize Firebase
+initializeApp(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
